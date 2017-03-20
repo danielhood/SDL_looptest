0faa77d0fdfa69dea9c1ea33324deab549a139ab
Daniel Hood <danielhood@hotmail.com>
2017-03-19 20:48:12 -0400

    Initial addition of positon based calculation of delays

--- src/looptest/reverb.c
+++ src/looptest/reverb.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "reverb.h"
 
 
@@ -14,16 +15,45 @@ static unsigned int delays[RESNUM][RESNUM-2];  // each source has a delay value 
 static short* sourceptr[RESNUM][RESNUM-2]; // list of pointers for each source
 
 
-void reverb_init() {
+// Positions of all resonators
+static int xpos[RESNUM];
+static int ypos[RESNUM];
+
+// Distance of primary ring
+static unsigned int r = 100000; 
+
+#define PI 3.1415
+
+void init_positions() {
+	// Initialize positons of resonators
+	// Currently just an evently distributed ring of resonators with source and output at center
+	// No stereo separation currently (likely will just separate L & R  channels by a fixed distance for both source and output
+
+	xpos[0] = 0;
+	ypos[0] = 0;
+	xpos[1] = 0;
+	xpos[1] = 0;
+
+	// Calcualte resonators (excluding source and output, which are at origin)
+	for (int i=2; i<RESNUM; i++) {
+		double a = 2 * PI / i-2;
+		xpos[i] = r * cos(a);
+		ypos[i] = r * sin(a);
+	}
+}
+
+void clear_mem() {
 	for(int i=0; i<RESNUM; i++) {
 		memset(outbfr[RESNUM], 0, MAX_DELAY_BUF_SIZE * sizeof(short));
 		memset(sources[RESNUM], 0, (RESNUM-2) * sizeof(short));
 		memset(delays[RESNUM], 0, (RESNUM-2) * sizeof(short));
 	}
+}
 
+void init_sources_static() {
 	// RESNUM0 is input; listens to no one
 	// RESNUM1 is output; listens to everyone except input and self; has no listeners
-	// RESNUM2+ is resonator; listens to everyone except self;
+	// RESNUM2+ is resonator; listens to everyone except self and output
 	sources[1][0] = 2;
 	sources[1][1] = 3;
 	sources[1][2] = 4;
@@ -39,7 +69,27 @@ void reverb_init() {
 	sources[4][0] = 0;
 	sources[4][1] = 2;
 	sources[4][2] = 3;
+}
 
+void init_sources() {
+	for (int i=1; i<RESNUM; i++) {
+		unsigned short curref;
+		if (i==1) {
+			curref = 2;
+		}
+		else {
+			curref = 0;
+		}
+
+		for (int j=0; j<RESNUM-2; j++) {
+			if (curref==1) curref++;
+			if (curref==i) curref++;
+			sources[i][j] = curref++;
+		}
+	}
+}
+
+void init_delays_static() {
 	delays[1][0] = 30000;
 	delays[1][1] = 50000;
 	delays[1][2] = 2900;
@@ -55,6 +105,36 @@ void reverb_init() {
 	delays[4][0] = 3600;
 	delays[4][1] = 37000;
 	delays[4][2] = 3200;
+}
+
+unsigned int calc_delay(short r1, short r2) {
+	// calculate delay based on distance between res1 and res2
+
+	int x1 = xpos[r1];
+	int y1 = ypos[r1];
+	int x2 = xpos[r2];
+	int y2 = ypos[r2];	
+	return 0;
+}
+
+void init_delays() {
+	// Set delays based on calcualted distance between resonators, including source and output
+
+	// Exclude input from source map
+	for(int i=1; i<RESNUM; i++) {
+		for (int j=0; j<RESNUM-2; j++) {
+			unsigned int delay = calc_delay(i, sources[i][j]);
+			delays[i][j] = delay;	
+		}
+	}
+}
+
+void reverb_init() {
+	clear_mem();
+	init_positions();
+
+	init_sources();
+	init_delays_static();
 
 	// Set initial outptrs
 	for (int i = 0; i < RESNUM; i++) {
@@ -85,7 +165,7 @@ void reverb_process(short* outputbfr, short* inputbfr, size_t numBytes) {
 			for (int j = 0; j < RESNUM-2; j++) {
 				val += *(sourceptr[i][j]);
 			}
-			*(outptr[i]) = (short)(val/5); // even mix for now
+			*(outptr[i]) = (short)(val/RESNUM); // even mix for now
 		}
 
 
